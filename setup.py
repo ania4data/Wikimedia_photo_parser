8e0e977aeca5094fed02320c48b3e426421da151
Anoosheh Niavarani <niavarani_a@hotmail.com>
2018-10-21 22:29:26 -0700

    add setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='distributions',
+setup(name='wikiphotoparser',
       version='0.1',
-      description='Gaussian distributions',
-      packages=['distributions'],
+      description='Download photo, author, credit, links from wikimedia common by parsing pages',
+      packages=['wikiphotoparser'],
       zip_safe=False)
