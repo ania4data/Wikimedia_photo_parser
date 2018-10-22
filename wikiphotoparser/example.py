b11bf91766b737d047bd8eb44e6222bfeeb0f694
Anoosheh Niavarani <niavarani_a@hotmail.com>
2018-10-22 01:44:37 -0700

    update example

--- wikiphotoparser/example.py
+++ wikiphotoparser/example.py
@@ -3,6 +3,7 @@ from getgloballinks import wiki_photo_parser
 
 
 wiki_parser = wiki_photo_parser()
+print(wiki_parser)
 wiki_parser.read_wiki_address('wiki_image_category_link.txt')
 print(wiki_parser.get_photo_address())
 print(wiki_parser.make_photo_collage())
