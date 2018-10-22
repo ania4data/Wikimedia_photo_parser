a0988d3c7e900bdf8d3852293423396f4952e37d
Anoosheh Niavarani <niavarani_a@hotmail.com>
2018-10-21 23:15:08 -0700

    update test.py

--- wikiphotoparser/test.py
+++ wikiphotoparser/test.py
@@ -3,18 +3,21 @@ from getgloballinks import wiki_photo_parser
 
 class Test_wikiphotoparser(unittest.TestCase):
 
-    def setUp(self):
+	def setUp(self):
 
-        self.wiki_photo_parser = wiki_photo_parser()
-        self.link_list = list('https://commons.wikimedia.org/wiki/Commons:Quality_images/Subject/Microscopic')
-        self.url_counter = 0
+		self.wiki_photo_parser = wiki_photo_parser()
+		self.wiki_photo_parser.read_wiki_address('wiki_image_category_link.txt')
+        #self.link_list = ['https://commons.wikimedia.org/wiki/Commons:Quality_images/Subject/Microscopic'.rstrip()]
+        #print(self.link_list)
+		self.url_counter = 0
 
-    def test_functions(self): 
 
+	def test_functions(self): 
 
-        self.assertEqual(elf.wiki_photo_parser.get_photo_address(self.link_list), 41, 'length of name string should be 41')
-        self.assertEqual(elf.wiki_photo_parser.make_photo_collage(), 41, 'length of index string should be 41')
-        self.assertEqual(elf.wiki_photo_parser.get_photos_infos(), 7, 'length of column of dataframe should be 7')
+		#link = ['https://commons.wikimedia.org/wiki/Commons:Quality_images/Subject/Microscopic'.rstrip()]
+		self.assertEqual(self.wiki_photo_parser.get_photo_address(), 41, 'length of name string should be 41')
+		self.assertEqual(self.wiki_photo_parser.make_photo_collage(), 100, 'length of index string should be 100')
+		self.assertEqual(self.wiki_photo_parser.get_photos_infos(), 7, 'length of column of dataframe should be 7')
 
 
     
