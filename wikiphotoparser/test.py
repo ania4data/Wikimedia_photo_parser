b1c4e1732b5a768b799efdb6885e579442a9ba90
Anoosheh Niavarani <niavarani_a@hotmail.com>
2018-10-22 02:03:59 -0700

    add more test

--- wikiphotoparser/test.py
+++ wikiphotoparser/test.py
@@ -7,16 +7,18 @@ class Test_wikiphotoparser(unittest.TestCase):
 
 		self.wiki_photo_parser = wiki_photo_parser()
 		self.wiki_photo_parser.read_wiki_address('wiki_image_category_link.txt')
-        #self.link_list = ['https://commons.wikimedia.org/wiki/Commons:Quality_images/Subject/Microscopic'.rstrip()]
-        #print(self.link_list)
+		self.text = self.wiki_photo_parser.read_wiki_address('wiki_image_category_link.txt')
 		self.url_counter = 0
 
 
-	def test_functions(self): 
+	def test_readingfile(self): 
+		self.assertEqual(self.text, 'https://commons.wikimedia.org/wiki/Commons:Quality_images/Subject/Microscopic', 'link does not match to Microscopic')
 
-		#link = ['https://commons.wikimedia.org/wiki/Commons:Quality_images/Subject/Microscopic'.rstrip()]
+	def test_parsingphoto(self):		
 		self.assertEqual(self.wiki_photo_parser.get_photo_address(), 41, 'length of name string should be 41')
+		
 		self.assertEqual(self.wiki_photo_parser.make_photo_collage(), 100, 'length of index string should be 100')
+		
 		self.assertEqual(self.wiki_photo_parser.get_photos_infos(), 7, 'length of column of dataframe should be 7')
 
 
